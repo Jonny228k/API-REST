4df2c4ccb5a1d8b99c908bdfbf26990e6850efa7
Dev Jonny <oliveira2k19@gmail.com>
2026-01-21 20:23:53 -0300

    Desenvolvimento do Delete e Put

--- src/index.js
+++ src/index.js
@@ -6,16 +6,26 @@ const app = express();
 app.use(express.json())
 
 //Mock do inventario
-const Inventario = [
-    {id: 1, nome: "Picareta", quantidade: 1, durabilidade: 52},
-    {id: 2, nome: "Tocha", quantidade: 6},
-    {id: 3, nome: "Espada", quantidade: 1, durabilidade: 30},
-    {id: 4, nome: "Mapa", quantidade: 1, durabilidade: 12},
+const inventario = [
+    { id: 1, nome: "Picareta", quantidade: 1, durabilidade: 52 },
+    { id: 2, nome: "Tocha", quantidade: 6 },
+    { id: 3, nome: "Espada", quantidade: 1, durabilidade: 30 },
+    { id: 4, nome: "Mapa", quantidade: 1, durabilidade: 12 },
 ]
 
+//retornar objeto pelo id
+function buscarItemporId(id) {
+    return inventario.filter(item => item.id == id)
+}
+
+//retorna a posicao do item no array
+function buscarIdporItem(id) {
+    return inventario.findIndex(item => item.id == id)
+}
+
 //criando uma rota padrão/raiz
-app.get("/", (req, res)=> {
-    res.send("Servidor online e operando na porta 3000")
+app.get("/", (req, res) => {
+    res.send("Bem-vindo ao Inventário do Minecraft!")
 })
 
 //Criando uma rota para o inventario
@@ -23,10 +33,32 @@ app.get("/inventario", (request, response) => {
     response.status(200).send(Inventario)
 })
 
+app.get("/inventario/:id", (request, response) => {
+    //let index = request.params.id
+    response.json(burcarItemporId(request.params.id))
+})
+
 //Criando um get.POST para que possa ser adicionado item no inventario
 app.post("/inventario", (request, response) => {
     Inventario.push(request.body),
-    response.status(201).send("Novo item adicionado com sucesso!")
+        response.status(201).send("Novo item adicionado com sucesso!")
 })
+
+app.delete("/inventario/:id", (req, res) => {
+    let index = buscarIdporItem(req.params.id)
+    Inventario.splice(index, 1)
+    res.status(200).send("Item removido com sucesso!")
+})
+
+// Put de atualização de item do inventario
+app.put("/inventario/:id", (req, res) => {
+    let index = buscarIdporItem(req.params.id)
+    inventario[index] = req.body.item
+    invenntario[index] = req.body.quantidade
+    inventario[index] = req.body.durabilidade
+    response.json(inventario)
+})
+
+
 
 export default app;
